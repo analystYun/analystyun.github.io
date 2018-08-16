fc5d595e5cc67e6600293dd6d691e4e352d12e9c
analystYun <ann.yun3@gmail.com>
2018-08-17 00:48:19 +0900

    Update vote.php

--- vote.php
+++ vote.php
@@ -1,1 +1,10 @@
-
+<html>
+  <head>
+    <meta http-equiv='Content-Type' content='text/html:charset=utf-8'>
+  </head>
+  <body>
+  <?php
+    echo $_REQUEST['value'] '번이 제출되었습니다. 감사합니다. ' ; 
+    ?>  
+  </body>
+</html>
